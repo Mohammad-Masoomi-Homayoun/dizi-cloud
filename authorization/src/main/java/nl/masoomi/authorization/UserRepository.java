039a0fcdb46b23e894cb527e1d89a5cb2ba68590
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2025-08-10 13:57:18 +0200

    Chore: stage 4, implemented authorization server application yaml config

--- authorization/src/main/java/nl/masoomi/authorization/UserRepository.java
+++ authorization/src/main/java/nl/masoomi/authorization/UserRepository.java
@@ -1,14 +1,14 @@
-package nl.masoomi.authorization;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserRepository extends UserDetailsService, CrudRepository<User, Long> {
-
-    User findByUsername(String username);
-
-    @Override
-    default User loadUserByUsername(String username) {
-        return findByUsername(username);
-    }
-}
+//package nl.masoomi.authorization;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//
+//public interface UserRepository extends UserDetailsService, CrudRepository<User, Long> {
+//
+//    User findByUsername(String username);
+//
+//    @Override
+//    default User loadUserByUsername(String username) {
+//        return findByUsername(username);
+//    }
+//}
