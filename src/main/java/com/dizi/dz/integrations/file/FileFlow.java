b4edf1d5487a72a5c7a75529c893ef514b456b56
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2025-09-21 17:15:03 +0200

    Chore: Stage 2, replaced simple integration with DSL flow

--- src/main/java/com/dizi/dz/integrations/file/FileFlow.java
+++ src/main/java/com/dizi/dz/integrations/file/FileFlow.java
@@ -2,13 +2,10 @@ package com.dizi.dz.integrations.file;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.annotation.Transformer;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.core.GenericTransformer;
-import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.messaging.MessageChannel;
 
 import java.io.File;
 
@@ -18,29 +15,15 @@ public class FileFlow {
     private static final String PATH = "/Users/nlmxm4200/Documents/programming/workspace/dizi-cloud/src/main/resources";
 
     @Bean
-    public MessageChannel textInChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MessageChannel fileWriterChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
-    public GenericTransformer<String, String> upperCaseTransformer() {
-        return String::toUpperCase;
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "fileWriterChannel")
-    public FileWritingMessageHandler fileWriter() {
-        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(PATH));
-        handler.setExpectReply(false);
-        handler.setFileExistsMode(FileExistsMode.APPEND);
-        handler.setAppendNewLine(true);
-        return handler;
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlow
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(String::toUpperCase)
+                .handle(Files
+                        .outboundAdapter(new File(PATH))
+                        .fileExistsMode(FileExistsMode.APPEND)
+                        .appendNewLine(true))
+                .get();
     }
 
 }
